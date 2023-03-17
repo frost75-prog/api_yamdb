1bb492754f5511a79c12e258423a475d1614c2b9
Danil Mustafin <danil.mustafin921@gmail.com>
2023-03-17 23:46:33 +0500

    fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,8 +2,9 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoriesViewSet, GenresViewSet, TitleViewSet, \
-    ReviewViewSet, CommentViewSet
+from .views import (
+    CategoriesViewSet, GenresViewSet, TitleViewSet,
+    ReviewViewSet, CommentViewSet)
 
 api_router_v1 = DefaultRouter()
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import PageNumberPagination
 # from rest_framework.permissions import
@@ -58,63 +59,8 @@ class TitleViewSet(viewsets.ModelViewSet):
     serializer_class = TitleSerializer
     pagination_class = PageNumberPagination
     permission_classes = (IsAccountAdminOrReadOnly,)
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('category__slug', 'genre__slug', 'name', 'year',)
-
-    def create(self, request, *args, **kwargs):
-        """
-        Создание жанра.
-        """
-        user = request.user.is_staff
-        data = {
-            "name": request.POST.get('name', None),
-            "year": request.POST.get('year', None),
-            "description": request.POST.get('description', None),
-            "genre": request.POST.get('genre', None),
-            "category": request.POST.get('category', None),
-        }
-        serializer = self.serializer_class(data=data,
-                                           context={'author': user})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                data=serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(
-                data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, *args, **kwargs):
-        """
-        Частичное обновление информации о произведении.
-        """
-        user = request.user.is_staff
-        instance = get_object_or_404(Titles, pk=self.kwargs.get('titles_id'))
-        data = {
-            "name": request.POST.get('name', None),
-            "year": request.POST.get('year', None),
-            "description": request.POST.get('description', None),
-            "genre": request.POST.get('genre', None),
-            "category": request.POST.get('category', None),
-        }
-        serializer = self.serializer_class(instance=instance,
-                                           data=data,
-                                           context={'author': user},
-                                           partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                data=serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(
-                data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, *args, **kwargs):
-        """
-        Удаление произведения.
-        """
-        instance = get_object_or_404(Titles, pk=self.kwargs.get('titles_id'))
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+    filter_class = TitleFilter
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
